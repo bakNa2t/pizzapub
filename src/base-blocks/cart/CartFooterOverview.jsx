35f18f4048871204dfdb1299867b3c34cc37c489
bakNa2t <ok2.lock@gmail.com>
2024-05-16 09:51:19 +0300

    Added Cart components

---
+++ src/base-blocks/cart/CartFooterOverview.jsx
@@ -0,0 +1,13 @@
+function CartFooterOverview() {
+  return (
+    <div>
+      <p>
+        <span>3 pizzas</span>
+        <span>$19.97</span>
+      </p>
+      <a href="#">Open cart &rarr;</a>
+    </div>
+  );
+}
+
+export default CartFooterOverview;
