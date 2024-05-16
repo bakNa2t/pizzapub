f97e10b9d51620aa2bcd7afc5b72097dd3d2d43d
bakNa2t <ok2.lock@gmail.com>
2024-05-16 09:55:43 +0300

    Added Menu components

---
+++ src/base-blocks/menu/Menu.jsx
@@ -0,0 +1,9 @@
+function Menu() {
+  return (
+    <div>
+      <h1>PizzaHub Menu</h1>
+    </div>
+  );
+}
+
+export default Menu;
---
+++ src/base-blocks/menu/MenuItem.jsx
@@ -0,0 +1,18 @@
+function MenuItem({ pizzaObj }) {
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizzaObj;
+
+  return (
+    <li>
+      <img src={imageUrl} alt={name} />
+      <div>
+        <p>{name}</p>
+        <p>{ingredients.join(", ")}</p>
+        <div>
+          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default MenuItem;
