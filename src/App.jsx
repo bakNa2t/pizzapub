90ba02327696cf70e9af956ff28d6794a0c24429
bakNa2t <ok2.lock@gmail.com>
2024-05-17 18:14:53 +0300

    Update Error and added to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Menu, { loader as menuLoader } from "./base-blocks/menu/Menu";
 
 import MainPaige from "./ui-blocks/MainPage";
-import Menu, { loader as menuLoader } from "./base-blocks/menu/Menu";
-import Cart from "./base-blocks/cart/Cart";
-import OrderCreating from "./base-blocks/order/OrderCreating";
-import Order from "./base-blocks/order/Order";
 import AppLayoyt from "./ui-blocks/AppLayoyt";
+import Cart from "./base-blocks/cart/Cart";
+import Order from "./base-blocks/order/Order";
+import OrderCreating from "./base-blocks/order/OrderCreating";
+import Error from "./ui-blocks/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayoyt />,
+
     children: [
       { path: "/", element: <MainPaige /> },
-      { path: "/menu", element: <Menu />, loader: menuLoader },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <OrderCreating /> },
       { path: "/order/:orderId", element: <Order /> },
