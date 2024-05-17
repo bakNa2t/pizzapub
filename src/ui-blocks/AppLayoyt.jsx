643cbafedc400c2c5298a59de7583d5ea13ea48e
bakNa2t <ok2.lock@gmail.com>
2024-05-17 15:57:42 +0300

    Added Loader elem while fetching data

--- src/ui-blocks/AppLayoyt.jsx
+++ src/ui-blocks/AppLayoyt.jsx
@@ -1,11 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 import Header from "./Header";
 import CartFooterOverview from "../base-blocks/cart/CartFooterOverview";
+import Loader from "./Loader";
 
 function AppLayoyt() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <h1>Content of PizzaPub</h1>
