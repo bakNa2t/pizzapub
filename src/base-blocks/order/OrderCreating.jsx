fc087cf48a5d4a1a76f18a82306dd24e1bb362ee
bakNa2t <ok2.lock@gmail.com>
2024-05-19 17:54:37 +0300

    Update action func in OrderCreating

--- src/base-blocks/order/OrderCreating.jsx
+++ src/base-blocks/order/OrderCreating.jsx
@@ -35,7 +35,6 @@ const fakeCart = [
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
-  console.log(cart);
 
   return (
     <div>
@@ -73,6 +72,7 @@ function CreateOrder() {
         </div>
 
         <div>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <button>Order now</button>
         </div>
       </Form>
@@ -83,7 +83,15 @@ function CreateOrder() {
 export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  console.log(data);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
+
+  console.log(order);
+
   return null;
 }
 
