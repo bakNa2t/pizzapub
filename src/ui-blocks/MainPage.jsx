f927e764ff098844f876447cde226ddd022d1655
bakNa2t <ok2.lock@gmail.com>
2024-05-20 11:33:02 +0300

    Added .prettierrc

--- src/ui-blocks/MainPage.jsx
+++ src/ui-blocks/MainPage.jsx
@@ -1,7 +1,9 @@
 function MainPage() {
   return (
     <div>
-      <h1>Welcome to PizzaPub</h1>
+      <h1 className="text-center text-2xl font-semibold text-purple-500">
+        Welcome to PizzaPub
+      </h1>
       <p>Start your order the best pizza here</p>
     </div>
   );
