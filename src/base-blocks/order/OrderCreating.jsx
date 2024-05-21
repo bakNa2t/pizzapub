9b2e95ffa046062c3197ab4a8523612d1dfbee12
bakNa2t <ok2.lock@gmail.com>
2024-05-21 10:43:22 +0300

    Update styles for form's input fields used tailwind @layer components

--- src/base-blocks/order/OrderCreating.jsx
+++ src/base-blocks/order/OrderCreating.jsx
@@ -47,13 +47,13 @@ function OrderCreating() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
             {formErrors?.phone && <p>{formErrors.phone}</p>}
           </div>
         </div>
@@ -61,12 +61,7 @@ function OrderCreating() {
         <div>
           <label>Address</label>
           <div>
-            <input
-              type="text"
-              name="address"
-              required
-              className="w-full rounded-full border border-slate-400 px-4 py-2 text-sm outline-none transition-all duration-300 focus:outline-none focus:ring focus:ring-purple-400 focus:ring-opacity-50 md:px-8 md:py-3"
-            />
+            <input type="text" name="address" required className="input" />
           </div>
         </div>
 
