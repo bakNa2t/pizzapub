cde7310b648192950e63339e4c128818cca2d4b9
bakNa2t <ok2.lock@gmail.com>
2024-05-21 11:20:24 +0300

    Added Button to UserCreating

--- src/ui-blocks/Button.jsx
+++ src/ui-blocks/Button.jsx
@@ -2,13 +2,13 @@ import PropTypes from "prop-types";
 
 function Button({ children, disabled }) {
   Button.propTypes = {
-    children: PropTypes.node.isRequired,
-    disabled: PropTypes.bool.isRequired,
+    children: PropTypes.node,
+    disabled: PropTypes.bool,
   };
   return (
     <button
       disabled={disabled}
-      className="inline-block rounded-full bg-purple-400 px-4 py-3 font-semibold uppercase tracking-wide text-slate-900 transition-all duration-300 hover:bg-purple-300 focus:outline-none focus:ring focus:ring-purple-300 focus:ring-offset-2"
+      className="inline-block rounded-full bg-purple-400 px-4 py-2 font-semibold uppercase tracking-wide text-slate-900 transition-all duration-300 hover:bg-purple-300 focus:outline-none focus:ring focus:ring-purple-300 focus:ring-offset-2 sm:px-6 sm:py-3"
     >
       {children}
     </button>
