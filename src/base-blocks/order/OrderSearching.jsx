3e21a706d4be0862ba1572b1c4743aa64baf4ab9
bakNa2t <ok2.lock@gmail.com>
2024-05-21 09:35:57 +0300

    Update styles of input search in OrderSearchin

--- src/base-blocks/order/OrderSearching.jsx
+++ src/base-blocks/order/OrderSearching.jsx
@@ -19,6 +19,7 @@ function OrderSearching() {
         placeholder="Search by order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="w-36 rounded-full bg-purple-200 px-4 py-2 text-sm outline-none transition-all duration-300 placeholder:text-slate-600 focus:outline-none focus:ring focus:ring-purple-600 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
