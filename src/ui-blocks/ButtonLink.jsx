fc95d3ab279c0111a8c775508a91349a2fc7ec0a
bakNa2t <ok2.lock@gmail.com>
2024-05-21 11:51:40 +0300

    Update Error used button if to=-1 else ButtoLink

--- src/ui-blocks/ButtonLink.jsx
+++ src/ui-blocks/ButtonLink.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 function ButtonLink({ children, to }) {
@@ -7,11 +7,19 @@ function ButtonLink({ children, to }) {
     to: PropTypes.string,
   };
 
+  const navigate = useNavigate();
+  const className =
+    "my-4 inline-block rounded-full px-2 py-1 text-sm text-indigo-400 transition-all duration-300 hover:bg-slate-300 hover:text-indigo-600 disabled:cursor-not-allowed";
+
+  if (to === "-1")
+    return (
+      <button className={className} onClick={() => navigate(-1)}>
+        {children}
+      </button>
+    );
+
   return (
-    <Link
-      to={to}
-      className="my-4 inline-block rounded-full px-2 py-1 text-sm text-indigo-400 transition-all duration-300 hover:bg-slate-300 hover:text-indigo-600 disabled:cursor-not-allowed"
-    >
+    <Link to={to} className={className}>
       {children}
     </Link>
   );
--- src/ui-blocks/Error.jsx
+++ src/ui-blocks/Error.jsx
@@ -1,14 +1,15 @@
-import { useNavigate, useRouteError } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
+import ButtonLink from "./ButtonLink";
 
 function Error() {
-  const navigate = useNavigate();
   const error = useRouteError();
 
   return (
     <div>
       <h1>Something went wrong</h1>
       <p>{error.data || error.message}</p>
-      <button onClick={() => navigate(-1)}>Go back</button>
+
+      <ButtonLink to="-1">Go back</ButtonLink>
     </div>
   );
 }
