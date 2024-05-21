41168cb26702799927a230a921dbefe9a3ee3c70
bakNa2t <ok2.lock@gmail.com>
2024-05-21 12:29:16 +0300

    Update Loader styles

--- src/ui-blocks/Loader.jsx
+++ src/ui-blocks/Loader.jsx
@@ -1,5 +1,9 @@
 function Loader() {
-  return <div className="loader"></div>;
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-indigo-400/10 text-purple-500 backdrop-blur-sm">
+      <div className="loader"></div>
+    </div>
+  );
 }
 
 export default Loader;
