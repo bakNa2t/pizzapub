cf8afaafe4a9b5c43b6507c4c74a2a9f8b473cd8
bakNa2t <ok2.lock@gmail.com>
2024-05-22 14:44:57 +0300

    Update UserName displayed username state if not null

--- src/base-blocks/user/UserName.jsx
+++ src/base-blocks/user/UserName.jsx
@@ -1,7 +1,13 @@
+import { useSelector } from "react-redux";
+
 function UserName() {
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
   return (
     <div className="text-md hidden font-semibold drop-shadow-sm md:block">
-      Welcome, USERNAME
+      Welcome, {username}
     </div>
   );
 }
