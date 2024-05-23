d34a70c10fb9f728bc00b276c82021c54d4bd116
bakNa2t <ok2.lock@gmail.com>
2024-05-23 09:47:29 +0300

    Update handleSubmit func added dispatch username

--- src/base-blocks/user/UserCreating.jsx
+++ src/base-blocks/user/UserCreating.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { updateName } from "./userSlice";
+
 import Button from "../../ui-blocks/Button";
 
 function CreateUser() {
   const [username, setUsername] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!username) return;
+    dispatch(updateName(username));
+    navigate("/menu");
   }
 
   return (
