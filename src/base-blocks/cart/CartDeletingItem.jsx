a92a9a1fe894783df67870a6c5464287ec94607d
bakNa2t <ok2.lock@gmail.com>
2024-05-23 14:17:47 +0300

    Added CartDeletingItem to CartItem

---
+++ src/base-blocks/cart/CartDeletingItem.jsx
@@ -0,0 +1,22 @@
+import { useDispatch } from "react-redux";
+import { deleteItem } from "./cartSlice";
+
+import Button from "../../ui-blocks/Button";
+
+import PropTypes from "prop-types";
+
+function CartDeletingItem({ pizzaId }) {
+  CartDeletingItem.propTypes = {
+    pizzaId: PropTypes.string.isRequired,
+  };
+
+  const dispatch = useDispatch();
+
+  return (
+    <Button type="small" onClick={() => dispatch(deleteItem(pizzaId))}>
+      Delete
+    </Button>
+  );
+}
+
+export default CartDeletingItem;
--- src/base-blocks/cart/CartItem.jsx
+++ src/base-blocks/cart/CartItem.jsx
@@ -1,7 +1,7 @@
-import Button from "../../ui-blocks/Button";
 import { formatCurrency } from "../../utils/utilsFunctions";
 
 import PropTypes from "prop-types";
+import CartDeletingItem from "./CartDeletingItem";
 
 function CartItem({ item }) {
   CartItem.propTypes = {
@@ -13,12 +13,12 @@ function CartItem({ item }) {
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1 sm:mb-0">
-        {quantity}&times; {name} {pizzaId}
+        {quantity}&times; {name}
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
       </div>
+      <CartDeletingItem pizzaId={pizzaId} />
     </li>
   );
 }
