26ac7c7450a98db896c0e75d0ae762fb562aa019
bakNa2t <ok2.lock@gmail.com>
2024-05-23 14:02:33 +0300

    Styled CartEmpty

--- src/base-blocks/cart/CartEmpty.jsx
+++ src/base-blocks/cart/CartEmpty.jsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
+import ButtonLink from "../../ui-blocks/ButtonLink";
 
 function EmptyCart() {
   return (
-    <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+    <div className="px-6 py-4">
+      <ButtonLink to="/menu">&larr; Back to menu</ButtonLink>
 
-      <p>Your cart is still empty. Start adding some pizzas :)</p>
+      <p className="mt-8 font-semibold ">
+        Your cart is still empty. Start adding some pizzas 😥
+      </p>
     </div>
   );
 }
