fe938fa8affc54cd8d839b0da38c5892409e787d
bakNa2t <ok2.lock@gmail.com>
2024-05-24 10:37:57 +0300

    Update decreaseItemQuantity if quantity equal  0 applies deleteItem reduser

--- src/base-blocks/cart/cartSlice.js
+++ src/base-blocks/cart/cartSlice.js
@@ -36,6 +36,8 @@ const cartSlice = createSlice({
       const item = state.cart.find((item) => item.pizzaId === action.payload);
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearItem(state) {
       state.cart = [];
