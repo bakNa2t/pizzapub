fe98cc4144596c18c289af64587f95fc535a3019
bakNa2t <ok2.lock@gmail.com>
2024-05-27 09:32:34 +0300

    Updated porp styles TiShoppingCart icon in CartFooterOverview

--- src/base-blocks/cart/CartFooterOverview.jsx
+++ src/base-blocks/cart/CartFooterOverview.jsx
@@ -11,15 +11,15 @@ function CartFooterOverview() {
   if (!totalCartQuantity) return null;
 
   return (
-    <div className="flex items-center justify-between bg-slate-700 px-6 py-3 text-sm font-semibold uppercase text-slate-200 sm:px-10 md:text-base">
+    <div className="flex items-center justify-between bg-slate-700 px-6 py-3 text-sm font-semibold uppercase text-slate-200 sm:px-10 md:py-4 md:text-base">
       <p className="space-x-3 text-slate-300 sm:space-x-5">
         <span>{totalCartQuantity} pizzas</span>
         <span>{formatCurrency(totalCartPrice)}</span>
       </p>
-      <p className="flex items-center space-x-1 md:space-x-3">
-        <Link to="/cart">Open cart</Link>
+      <Link to="/cart" className="flex items-center gap-2 md:gap-4">
+        Open cart
         <TiShoppingCart className="text-xl text-purple-400 md:text-2xl" />
-      </p>
+      </Link>
     </div>
   );
 }
