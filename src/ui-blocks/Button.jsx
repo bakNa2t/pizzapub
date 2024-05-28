e57f541c24d35e3f7f786ddc2507ec986502b1ce
bakNa2t <ok2.lock@gmail.com>
2024-05-28 10:18:24 +0300

    Updated Button's type primary styles

--- src/ui-blocks/Button.jsx
+++ src/ui-blocks/Button.jsx
@@ -19,7 +19,7 @@ function Button({ children, disabled, to, type, onClick }) {
     round:
       base + " px-2.5 py-1 md:px-3.5 md:py-1.25 md:px-[.725rem] md:text-base",
     secondary:
-      "inline-block text-sm rounded-full border-2 border-slate-400 px-4 py-1.5 font-semibold uppercase tracking-wide text-slate-400 transition-all duration-300  hover:bg-slate-400 hover:text-slate-800 focus::bg-slate-400 focus:outline-none focus:text-slate-800 focus:ring focus:ring-slate-400 focus:ring-offset-2 md:px-6 md:py-2.5 md:text-base",
+      "inline-block text-sm rounded-full border-2 border-slate-400 px-4 py-2.5 font-semibold uppercase tracking-wide text-slate-400 transition-all duration-300  hover:bg-slate-400 hover:text-slate-800 focus:bg-slate-400 focus:outline-none focus:text-slate-800 focus:ring focus:ring-slate-400 focus:ring-offset-2 md:px-6 md:py-2.5 md:text-base",
   };
 
   if (to)
