909e5faae318449c6a07d0b1d6866ae5a4e51f1d
bakNa2t <ok2.lock@gmail.com>
2024-05-29 16:47:38 +0300

    Added HiOutlineSearch to search input of OrderSearching

--- src/base-blocks/order/OrderSearching.jsx
+++ src/base-blocks/order/OrderSearching.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
 import { useNavigate } from "react-router-dom";
 
 function OrderSearching() {
@@ -14,13 +15,14 @@ function OrderSearching() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="relative">
       <input
         placeholder="Enter your order #..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="w-40 rounded-full bg-purple-200 px-4 py-2 text-xs outline-none transition-all duration-300 placeholder:italic placeholder:text-purple-600 focus:outline-none focus:ring focus:ring-purple-600 focus:ring-opacity-50 sm:w-64 sm:text-sm sm:focus:w-72"
+        className="w-5 rounded-full bg-purple-200 px-4 py-2 pl-8 text-xs outline-none transition-all duration-300 placeholder:italic placeholder:text-transparent focus:w-56 focus:outline-none focus:ring focus:ring-purple-600 focus:ring-opacity-50 focus:placeholder:text-purple-600 sm:w-64 sm:text-sm sm:placeholder:text-purple-600 sm:focus:w-80"
       />
+      <HiOutlineSearch className="absolute left-2 top-1/2 h-5 w-5 -translate-y-1/2 text-purple-900" />
     </form>
   );
 }
