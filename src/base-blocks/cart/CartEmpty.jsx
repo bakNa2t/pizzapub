9e3364b48914dcf88d27a295afd1a56e511faad2
bakNa2t <ok2.lock@gmail.com>
2024-05-31 11:19:18 +0300

    Added TiArrowBackOutline to ButtonLink to CartEmpty

--- src/base-blocks/cart/CartEmpty.jsx
+++ src/base-blocks/cart/CartEmpty.jsx
@@ -1,9 +1,12 @@
+import { TiArrowBackOutline } from "react-icons/ti";
 import ButtonLink from "../../ui-blocks/ButtonLink";
 
 function EmptyCart() {
   return (
     <div className="px-6 py-4">
-      <ButtonLink to="/menu">&larr; Back to menu</ButtonLink>
+      <ButtonLink to="/menu">
+        <TiArrowBackOutline className="h-5 w-5" /> Back to menu
+      </ButtonLink>
 
       <p className="mt-8 font-semibold ">
         Your cart is still empty. Start adding some pizzas 😥
