8664013854817b35785c35ac1556d6224befc886
bakNa2t <ok2.lock@gmail.com>
2024-06-02 12:08:36 +0300

    Added LinkSide to Footer

--- src/ui-blocks/Footer.jsx
+++ src/ui-blocks/Footer.jsx
@@ -1,3 +1,5 @@
+import LinkSide from "./LinkSide";
+
 function Footer() {
   function fullYear() {
     return new Date().getFullYear();
@@ -7,13 +9,13 @@ function Footer() {
     <footer className="absolute bottom-2 left-1/2 -translate-x-1/2 text-slate-500">
       <p className="text-xs">
         &copy; Copyright {fullYear()}. <strong>PizzaPub</strong> designed by{" "}
-        <a
+        <LinkSide
           href="https://github.com/bakna2t"
           target="_blank"
           rel="noopener noreferrer"
         >
           _&#216;k
-        </a>
+        </LinkSide>
       </p>
     </footer>
   );
