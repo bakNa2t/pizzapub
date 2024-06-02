4b76c62766ac0a881df3fdc5aebf35d5ea234ba8
bakNa2t <ok2.lock@gmail.com>
2024-06-02 12:39:48 +0300

    Hided Footer if totalCartQuantity exsist

--- src/ui-blocks/Footer.jsx
+++ src/ui-blocks/Footer.jsx
@@ -1,9 +1,16 @@
+import { useSelector } from "react-redux";
+import { getTotalCartQuantity } from "../base-blocks/cart/cartSlice";
+
 import LinkSide from "./LinkSide";
 
 function Footer() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+
   function fullYear() {
     return new Date().getFullYear();
   }
+
+  if (totalCartQuantity) return null;
 
   return (
     <footer className="absolute bottom-2 left-1/2 -translate-x-1/2 text-slate-500">
