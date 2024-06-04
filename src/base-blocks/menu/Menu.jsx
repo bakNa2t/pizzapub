581b0ab8a5b93b34fec5d0a9e0c222551f45fcd1
bakNa2t <ok2.lock@gmail.com>
2024-06-04 14:32:06 +0300

    Added scrollTop state to Menu

--- src/base-blocks/menu/Menu.jsx
+++ src/base-blocks/menu/Menu.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import { getMenu } from "../../services/apiFakePizzaMenu";
+import { useEffect, useState } from "react";
 
 import MenuItem from "./MenuItem";
 import ButtonToTop from "../../ui-blocks/ButtonToTop";
@@ -7,6 +8,29 @@ import ButtonToTop from "../../ui-blocks/ButtonToTop";
 function Menu() {
   const menu = useLoaderData();
   // console.log(menu);
+  function returnToTop() {
+    document.body.top = 0;
+    document.documentElement.scrollTop = 0;
+    console.log("Resulst:");
+  }
+
+  const [scrollTop, setScrollTop] = useState(0);
+
+  useEffect(
+    function () {
+      function handleScroll() {
+        setScrollTop(window.scrollY);
+      }
+
+      window.addEventListener("scroll", handleScroll);
+
+      console.log(scrollTop);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    },
+    [scrollTop],
+  );
 
   return (
     <>
@@ -15,7 +39,7 @@ function Menu() {
           <MenuItem pizzaObj={pizza} key={pizza.id} />
         ))}
       </ul>
-      <ButtonToTop />
+      <ButtonToTop onClick={returnToTop} />
     </>
   );
 }
--- src/ui-blocks/ButtonToTop.jsx
+++ src/ui-blocks/ButtonToTop.jsx
@@ -1,10 +1,18 @@
 import { IoIosArrowUp } from "react-icons/io";
+import PropTypes from "prop-types";
 
-function ButtonToTop() {
+function ButtonToTop({ onClick }) {
+  ButtonToTop.propTypes = {
+    onClick: PropTypes.func,
+  };
+
   return (
-    <div className="fixed bottom-14 right-3 h-6 w-6 cursor-pointer rounded-full bg-slate-700 p-[5px] text-slate-200 opacity-40 transition-all duration-300 hover:text-slate-200 hover:opacity-100 sm:bottom-14 sm:right-10 sm:h-10 sm:w-10 sm:rounded-none sm:p-2 md:bottom-20">
+    <button
+      onClick={onClick}
+      className="fixed bottom-14 right-3 h-6 w-6 rounded-full bg-slate-700 p-[5px] text-slate-200 opacity-40 transition-all duration-300 hover:text-slate-200 hover:opacity-100 sm:bottom-14 sm:right-10 sm:h-10 sm:w-10 sm:rounded-none sm:p-2 md:bottom-20"
+    >
       <IoIosArrowUp className="text-xs font-bold sm:text-2xl" />
-    </div>
+    </button>
   );
 }
 
