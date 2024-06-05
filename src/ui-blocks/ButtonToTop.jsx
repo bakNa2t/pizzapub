b7da67eed57c59a08fa0b95eb32884e93dad4e6d
bakNa2t <ok2.lock@gmail.com>
2024-06-05 14:32:38 +0300

    Updated shadow tyles to ButtonToTop

--- src/ui-blocks/ButtonToTop.jsx
+++ src/ui-blocks/ButtonToTop.jsx
@@ -7,34 +7,10 @@ function ButtonToTop({ onClick }) {
     onClick: PropTypes.func,
   };
 
-  /*useEffect(function () {
-    const onScroll = () => setScrollTop(window.scrollY);
-    // clean up code
-    window.removeEventListener("scroll", onScroll);
-    window.addEventListener("scroll", onScroll, { passive: true });
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);*/
-
-  // useEffect(
-  //   function () {
-  //     function handleScroll(e) {
-  //       setScrollTop(e.currentTarget.scrollY);
-  //     }
-
-  //     window.addEventListener("scroll", handleScroll);
-
-  //     console.log(scrollTop);
-  //     return () => {
-  //       window.removeEventListener("scroll", handleScroll);
-  //     };
-  //   },
-  //   [scrollTop],
-  // );
-
   return (
     <button
       onClick={onClick}
-      className="fixed bottom-14 right-3 h-6 w-6 rounded-full bg-slate-700 p-[5px] text-slate-200 opacity-40 transition-all duration-300 hover:text-slate-200 hover:opacity-100 sm:bottom-14 sm:right-10 sm:h-10 sm:w-10 sm:rounded-none sm:p-2 md:bottom-20"
+      className="fixed bottom-14 right-3 h-6 w-6 rounded-full bg-slate-700 p-[5px] text-slate-200 opacity-20 transition-all duration-300 hover:text-slate-200 hover:opacity-100 sm:bottom-14 sm:right-6 sm:h-10 sm:w-10 sm:rounded-lg sm:p-2 md:bottom-20 md:hover:shadow-md md:hover:shadow-slate-600/80"
     >
       <IoIosArrowUp className="text-xs font-bold sm:text-2xl" />
     </button>
