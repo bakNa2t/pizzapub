fff6887e928056d47bbf96a46a7a547fae01b30c
bakNa2t <ok2.lock@gmail.com>
2024-06-07 11:59:25 +0300

    Added bg image and updated prop styles

--- src/ui-blocks/AppLayoyt.jsx
+++ src/ui-blocks/AppLayoyt.jsx
@@ -10,7 +10,7 @@ function AppLayoyt() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="relative grid h-screen_dyn grid-rows-[auto_1fr_auto]">
+    <div className="relative grid h-screen_dyn grid-rows-[auto_1fr_auto] before:absolute before:left-0 before:top-0 before:z-[-1] before:h-screen before:w-full before:bg-cover before:bg-fixed before:bg-center before:bg-no-repeat before:opacity-[0.4] before:content-[bg-[url('/images/pizzapub-bg.jpg')]]">
       {isLoading && <Loader />}
       <Header />
       <main className="overflow-y-scroll">
