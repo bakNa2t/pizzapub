8fe911b6640411574d20d81bf8b8bee50cd7945b
bakNa2t <ok2.lock@gmail.com>
2024-06-10 13:59:42 +0300

    Updated z-index prop style in Loader

--- src/ui-blocks/Loader.jsx
+++ src/ui-blocks/Loader.jsx
@@ -1,6 +1,6 @@
 function Loader() {
   return (
-    <div className="absolute inset-0 flex items-center justify-center bg-indigo-400/10 text-purple-500 backdrop-blur-sm">
+    <div className="absolute inset-0 z-50 flex items-center justify-center bg-indigo-400/10 text-purple-500 backdrop-blur-sm">
       <div className="loader"></div>
     </div>
   );
